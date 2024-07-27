f6f861f20bc68aa2265f5f6d55a27192863be5f8
Muhammad Hanif <hanifeij@yahoo.co.id>
2024-07-27 23:10:14 +0700

    feat: add tab layering

--- rules.ts
+++ rules.ts
@@ -1,7 +1,14 @@
 import fs from "fs";
 import { KarabinerRules } from "./types";
-import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
-import { deepmerge as merge } from "deepmerge-ts";
+import {
+  createHyperSubLayers,
+  app,
+  open,
+  rectangle,
+  shell,
+  createTabSubLayers,
+  resetTabOnKeys,
+} from "./utils";
 
 const rules: KarabinerRules[] = [
   {
@@ -29,6 +36,111 @@ const rules: KarabinerRules[] = [
       },
     ],
   },
+  {
+    description: "Use ctrl with backslash",
+    manipulators: [
+      {
+        description: "to ctrl",
+        from: {
+          key_code: "backslash",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "left_control",
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "backslash",
+          },
+        ],
+        type: "basic",
+      },
+    ],
+  },
+  {
+    description: "Tab Layering",
+    manipulators: [
+      {
+        description: "Tab Layering",
+        from: {
+          key_code: "tab",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            set_variable: {
+              name: "tab_layer",
+              value: 1,
+            },
+          },
+        ],
+        to_after_key_up: [
+          {
+            set_variable: {
+              name: "tab_layer",
+              value: 0,
+            },
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "tab",
+          },
+        ],
+        type: "basic",
+      },
+    ],
+  },
+  ...createTabSubLayers({
+    ...resetTabOnKeys([
+      "left_command",
+      "left_control",
+      "right_command",
+      "right_control",
+    ]),
+    h: {
+      to: [
+        {
+          key_code: "left_arrow",
+        },
+      ],
+    },
+    j: {
+      to: [
+        {
+          key_code: "down_arrow",
+        },
+      ],
+    },
+    k: {
+      to: [
+        {
+          key_code: "up_arrow",
+        },
+      ],
+    },
+    l: {
+      to: [
+        {
+          key_code: "right_arrow",
+        },
+      ],
+    },
+    q: {
+      to: [
+        {
+          key_code: "left_control",
+        },
+      ],
+    },
+  }),
+
   // Define the Hyper key itself
   {
     description: "Hyper Key (⌃⌥⇧⌘)",
@@ -64,46 +176,58 @@ const rules: KarabinerRules[] = [
         ],
         type: "basic",
       },
-      //      {
-      //        type: "basic",
-      //        description: "Disable CMD + Tab to force Hyper Key usage",
-      //        from: {
-      //          key_code: "tab",
-      //          modifiers: {
-      //            mandatory: ["left_command"],
-      //          },
-      //        },
-      //        to: [
-      //          {
-      //            key_code: "tab",
-      //          },
-      //        ],
-      //      },
     ],
   },
   ...createHyperSubLayers({
     // task related
-    t: {
-      // pomodoro
-      p: open("raycast://extensions/asubbotin/pomodoro/pomodoro-control-timer"),
-    },
     // open
     o: {
       c: app("Google Chrome"),
+      l: app("Calendar"),
+      f: app("Figma"),
       t: app("iTerm"),
       b: app("Obsidian"),
+      spacebar: open(
+        "raycast://extensions/KevinBatdorf/obsidian/dailyNoteCommand"
+      ),
+      // pomodoro
+      p: app("Session"),
       s: app("Slack"),
-      l: app("Mail"),
+      m: app("Mail"),
+      v: app("Visual Studio Code"),
     },
-    // window management
     w: {
+      // window management
       l: rectangle("left-half"),
       k: rectangle("first-two-thirds"),
       e: rectangle("last-two-thirds"),
       t: rectangle("last-third"),
       r: rectangle("right-half"),
+      semicolon: rectangle("first-third"),
       m: rectangle("maximize"),
       c: rectangle("center"),
+    },
+    r: {
+      // raycast related
+      h: open(
+        "raycast://extensions/raycast/clipboard-history/clipboard-history"
+      ),
+      s: open("raycast://extensions/raycast/snippets/search-snippets"),
+      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
+      spacebar: open("raycast://extensions/raycast/raycast/confetti"),
+      c: open("raycast://extensions/raycast/calendar/my-schedule"),
+      j: open("raycast://extensions/raycast/jira/active-sprints"),
+      q: open("raycast://extensions/Codely/google-chrome/search-tab"),
+    },
+    // terminal related
+    t: {
+      w: open(
+        "raycast://extensions/louishuyng/tmux-sessioner/manage_tmux_windows"
+      ),
+      s: open("raycast://extensions/louishuyng/tmux-sessioner/index"),
+      c: open(
+        "raycast://extensions/louishuyng/tmux-sessioner/create_new_session"
+      ),
     },
     v: {
       h: {
@@ -160,7 +284,7 @@ const currentProfileAndRule = JSON.parse(
   fs.readFileSync("karabiner.json", "utf8")
 );
 currentProfileAndRule.profiles[0].complex_modifications.rules = rules;
-console.log(currentProfileAndRule);
+
 fs.writeFileSync(
   "karabiner.json",
   JSON.stringify(currentProfileAndRule, null, 2)
--- utils.ts
+++ utils.ts
@@ -13,6 +13,172 @@ type HyperKeySublayer = {
   [key_code in KeyCode]?: LayerCommand;
 };
 
+interface TabResetObject {
+  key: KeyCode;
+  manipulator: Manipulator;
+}
+
+export function resetTabOnKeys(keys: KeyCode[]): any {
+  return keys
+    .map(
+      (key: KeyCode) =>
+        ({
+          key,
+          manipulator: {
+            to: [
+              {
+                key_code: "tab",
+              },
+            ],
+          },
+        } as TabResetObject)
+    )
+    .reduce((prev, next: TabResetObject) => {
+      prev[next.key] = next.manipulator;
+      return prev;
+    }, {} as Record<KeyCode, Manipulator>);
+}
+
+/**
+ * Create a Hyper Key sublayer, where every command is prefixed with a key
+ * e.g. Hyper + O ("Open") is the "open applications" layer, I can press
+ * e.g. Hyper + O + G ("Google Chrome") to open Chrome
+ */
+export function createTabSubLayer(
+  sublayer_key: KeyCode,
+  commands: HyperKeySublayer,
+  allSubLayerVariables: string[]
+): Manipulator[] {
+  const subLayerVariableName = generateSubLayerVariableName(
+    sublayer_key,
+    "tab_sublayer_"
+  );
+
+  return [
+    // When Hyper + sublayer_key is pressed, set the variable to 1; on key_up, set it to 0 again
+    {
+      description: `Toggle Tab sublayer ${sublayer_key}`,
+      type: "basic",
+      from: {
+        key_code: sublayer_key,
+        modifiers: {
+          optional: ["any"],
+        },
+      },
+      to_after_key_up: [
+        {
+          set_variable: {
+            name: subLayerVariableName,
+            // The default value of a variable is 0: https://karabiner-elements.pqrs.org/docs/json/complex-modifications-manipulator-definition/conditions/variable/
+            // That means by using 0 and 1 we can filter for "0" in the conditions below and it'll work on startup
+            value: 0,
+          },
+        },
+      ],
+      to: [
+        {
+          set_variable: {
+            name: subLayerVariableName,
+            value: 1,
+          },
+        },
+      ],
+      // This enables us to press other sublayer keys in the current sublayer
+      // (e.g. Hyper + O > M even though Hyper + M is also a sublayer)
+      // basically, only trigger a sublayer if no other sublayer is active
+      conditions: [
+        ...allSubLayerVariables
+          .filter(
+            (subLayerVariable) => subLayerVariable !== subLayerVariableName
+          )
+          .map((subLayerVariable) => ({
+            type: "variable_if" as const,
+            name: subLayerVariable,
+            value: 0,
+          })),
+        {
+          type: "variable_if",
+          name: "hyper",
+          value: 1,
+        },
+      ],
+    },
+    // Define the individual commands that are meant to trigger in the sublayer
+    ...(Object.keys(commands) as (keyof typeof commands)[]).map(
+      (command_key): Manipulator => ({
+        ...commands[command_key],
+        type: "basic" as const,
+        from: {
+          key_code: command_key,
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        // Only trigger this command if the variable is 1 (i.e., if Hyper + sublayer is held)
+        conditions: [
+          {
+            type: "variable_if",
+            name: subLayerVariableName,
+            value: 1,
+          },
+        ],
+      })
+    ),
+  ];
+}
+
+/**
+ * Create all hyper sublayers. This needs to be a single function, as well need to
+ * have all the hyper variable names in order to filter them and make sure only one
+ * activates at a time
+ */
+export function createTabSubLayers(subLayers: {
+  [key_code in KeyCode]?: HyperKeySublayer | LayerCommand;
+}): KarabinerRules[] {
+  const allSubLayerVariables = (
+    Object.keys(subLayers) as (keyof typeof subLayers)[]
+  ).map((sublayer_key) => generateSubLayerVariableName(sublayer_key));
+
+  return Object.entries(subLayers).map(([key, value]) =>
+    "to" in value
+      ? {
+          description: `Tab Key + ${key}`,
+          manipulators: [
+            {
+              ...value,
+              type: "basic" as const,
+              from: {
+                key_code: key as KeyCode,
+                modifiers: {
+                  optional: ["any"],
+                },
+              },
+              conditions: [
+                {
+                  type: "variable_if",
+                  name: "tab_layer",
+                  value: 1,
+                },
+                ...allSubLayerVariables.map((subLayerVariable) => ({
+                  type: "variable_if" as const,
+                  name: subLayerVariable,
+                  value: 0,
+                })),
+              ],
+            },
+          ],
+        }
+      : {
+          description: `Tab Key sublayer "${key}"`,
+          manipulators: createHyperSubLayer(
+            key as KeyCode,
+            value,
+            allSubLayerVariables
+          ),
+        }
+  );
+}
+
 /**
  * Create a Hyper Key sublayer, where every command is prefixed with a key
  * e.g. Hyper + O ("Open") is the "open applications" layer, I can press
@@ -150,8 +316,11 @@ export function createHyperSubLayers(subLayers: {
   );
 }
 
-function generateSubLayerVariableName(key: KeyCode) {
-  return `hyper_sublayer_${key}`;
+function generateSubLayerVariableName(
+  key: KeyCode,
+  layer_name: string = "hyper_sublayer_"
+) {
+  return `${layer_name}${key}`;
 }
 
 /**
